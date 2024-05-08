254b871586d60daf7b655b4a5bf4f1421910dde4
Wan Muzaffar <wanmuz86@gmail.com>
2024-05-08 14:33:33 +0800

    set up the MovieDetail class

---
+++ lib/models/movie_detail.dart
@@ -0,0 +1,42 @@
+// 1) Create the class
+class MovieDetail {
+  // 2) Create the properties
+  final String title;
+  final String poster;
+  final String imdbId;
+  final String type;
+  final String year;
+  final String genre;
+  final String? actors;
+  final String plot;
+  final String released;
+  final String director;
+  // 3) Generate the constructor
+
+  MovieDetail(
+      {required this.title,
+      required this.poster,
+      required this.imdbId,
+      required this.type,
+      required this.year,
+      required this.genre,
+        this.actors,
+      required this.plot,
+      required this.released,
+      required this.director});
+
+  // 4) fromJson method (JSON to Object transformer)
+
+factory MovieDetail.fromJson(Map<String,dynamic> json){
+  return MovieDetail(title: json["Title"],
+      poster: json["Poster"],
+      imdbId: json["imdbID"],
+      type: json["Type"],
+      year: json["Year"],
+      genre: json["Genre"],
+      plot: json["Plot"],
+      released: json["Released"],
+      director: json["Director"],
+      actors: json["Actors"]);
+}
+}
